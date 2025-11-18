429d6e5b2831a8943e2871611f71fafea72dcca7
Pietro <itachinhorp@gmail.com>
2025-11-17 22:15:53 -0300

    Initial commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using ApiPostagens.Models;
+using Newtonsoft.Json;
+
+namespace ApiPostagens
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async private void BuscarBtn_Clicked(object sender, EventArgs e)
+        {
+
+            Postagem minhaPostagem = new Postagem();
+
+            string URI = "https://jsonplaceholder.typicode.com/posts";
+
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage responseMessage = await httpClient.GetAsync(URI);
+
+            List<Postagem> postagens = new List<Postagem>();
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                string conteudo = await responseMessage.Content.ReadAsStringAsync();
+
+                postagens = JsonConvert.DeserializeObject<List<Postagem>>(conteudo);
+
+                UserIdLbl.Text = Convert.ToString(postagens[0].UserId);
+                IdLbl.Text = Convert.ToString(postagens[0].Id);
+                TitleLbl.Text = postagens[0].Title;
+                BodyLbl.Text = postagens[0].Body;
+            }
+        }
+    }
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ApiPostagens
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
